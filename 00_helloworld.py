1fbeae27a6ed8d1224b5556b88b8e38b60f3e997
Vinod Kumar <vinodkumar@192.168.1.7>
2025-07-04 19:41:58 +0530

    new file:   00_helloworld.py

--- 00_helloworld.py
+++ 00_helloworld.py
@@ -1,25 +1,9 @@
+print("This line will be printed.")
 
-print("Hola Python")
-print('Hola Python')
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
 
-# Esto es un comentario
 
-"""
-Este es un
-comentario
-en varias líneas
-"""
-
-'''
-Este también es un
-comentario
-en varias líneas
-'''
-
-# Cómo consultar el tipo de dato
-print(type("Soy un dato str"))  # Tipo 'str'
-print(type(5))  # Tipo 'int'
-print(type(1.5))  # Tipo 'float'
-print(type(3 + 1j))  # Tipo 'complex'
-print(type(True))  # Tipo 'bool'
-print(type(print("Mi cadena de texto")))  # Tipo 'NoneType'
+print("Goodbye, World!")
